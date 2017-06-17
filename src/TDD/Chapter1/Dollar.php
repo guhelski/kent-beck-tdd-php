57491f488905169393c5a96afbf604cb8b983a38
Logan Connor <loganconnor44@gmail.com>
2017-06-17 15:22:16 -0400

    Added PHP7 type declarations and added comments to methods

--- src/TDD/Chapter1/Dollar.php
+++ src/TDD/Chapter1/Dollar.php
@@ -4,14 +4,28 @@ namespace TDD\Chapter1;
 
 class Dollar
 {
+    
+    /**
+     * @var int $amount The monetary amount given to the dollar object
+     */
     public $amount;
 
-    public function __construct($amount)
+    /**
+     * Instantiate the dollar class with an amount value
+     * 
+     * @param int $amount
+     */
+    public function __construct(int $amount)
     {
         $this->amount = $amount;
     }
 
-    public function times($multiplier)
+    /**
+     * Takes a multipler to set the product of itself and the public $amount variable
+     * 
+     * @param int $multiplier
+     */
+    public function times(int $multiplier)
     {
         $this->amount *= $multiplier;
     }
