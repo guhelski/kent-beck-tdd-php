4a9aa7fae95ec7503bc3680e86f0ffe12f4830ea
Logan Connor <loganconnor44@gmail.com>
2017-06-17 15:29:30 -0400

    corrected curlys to original developer's style preference

--- tests/TDD/Chapter1/Chapter1Test.php
+++ tests/TDD/Chapter1/Chapter1Test.php
@@ -2,12 +2,14 @@
 
 namespace TDD\Chapter1;
 
-class Chapter1Test extends TestCase {
+class Chapter1Test extends TestCase 
+{
 
 	/**
 	 * Tests that Dollar's public variable, $amount, is the same as the given amount
 	 */
-    public function testMultiplication() {
+    public function testMultiplication() 
+    {
         $five = new Dollar(5);
         $five->times(2);
         $this->assertEquals(10, $five->amount);
