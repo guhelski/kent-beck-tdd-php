2eb61a414ac0ea2d1d99bdf82a724cfd22f0b9ad
Logan Connor <loganconnor44@gmail.com>
2017-06-17 16:39:42 -0400

    Added PHP7 type declarations and added comments to methods & variables

--- src/TDD/Chapter4/Dollar.php
+++ src/TDD/Chapter4/Dollar.php
@@ -4,18 +4,41 @@ namespace TDD\Chapter4;
 
 class Dollar
 {
+
+	/**
+     * @var int $amount The monetary amount given to the dollar object
+     */
 	private $amount;
 
+	/**
+     * Instantiate the dollar class with an amount value
+     * 
+     * @param int $amount
+     */
 	public function __construct($amount)
 	{
 		$this->amount = $amount;
 	}
 
+	/**
+     * Takes a multipler to returns a new Dollar object instantiated with the product of the $amount and the passed in $multiplier
+     * 
+     * @param int $multiplier
+     * @return object Dollar
+     */
 	public function times($multiplier)
 	{
 		return new Dollar($this->amount * $multiplier); 
 	}
 
+	/**
+     * Take a Dollar object and returns a boolean if the passed in Dollar's $amount matches the self $amount
+     * @todo remove redundant renaming of $object to $dollar
+     * @todo capitalize $dollar for better readability to show it is an object
+     * 
+     * @param Dollar $object
+     * @return bool
+     */
 	public function equals(Dollar $object)
 	{
 		$dollar = $object;
