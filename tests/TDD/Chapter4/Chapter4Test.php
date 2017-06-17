1437cfcd08c9cbd5171472a439f56182e90ce857
Logan Connor <loganconnor44@gmail.com>
2017-06-17 16:39:42 -0400

    Added PHP7 type declarations and added comments to methods & variables

--- tests/TDD/Chapter4/Chapter4Test.php
+++ tests/TDD/Chapter4/Chapter4Test.php
@@ -4,6 +4,10 @@ namespace TDD\Chapter4;
 
 class Chapter4Test extends \PHPUnit_Framework_TestCase
 {
+
+    /**
+     * Tests that the object $product's method amount returns the expected value
+     */
     public function testMultiplication()
     {
         $five = new Dollar(5);
@@ -11,6 +15,9 @@ class Chapter4Test extends \PHPUnit_Framework_TestCase
         $this->assertEquals(new Dollar(15), $five->times(3));
     }
 
+    /**
+     * Using Dollar's method equal, this tests that both objects are equal to another returning a true
+     */
     public function testEquality()
     {
     	$fiveA = new Dollar(5);
