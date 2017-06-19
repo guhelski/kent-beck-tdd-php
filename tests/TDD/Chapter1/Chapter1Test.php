a3c20ecf851230010e1888b165972b911a764e98
Logan Connor <loganconnor44@gmail.com>
2017-06-19 16:54:17 -0400

    Attempting to correct strange spacing

--- tests/TDD/Chapter1/Chapter1Test.php
+++ tests/TDD/Chapter1/Chapter1Test.php
@@ -7,7 +7,7 @@ class Chapter1Test extends TestCase
 	/**
 	 * Tests that Dollar's public variable, $amount, is the same as the given amount
 	 */
-    public function testMultiplication() 
+	public function testMultiplication() 
     {
         $five = new Dollar(5);
         $five->times(2);
