b9e7df18a412d54f72afad731c88dcd2036fdf86
Logan Connor <loganconnor44@gmail.com>
2017-06-26 12:23:19 -0400

    correcting spacing issue

--- src/TDD/Chapter10/Dollar.php
+++ src/TDD/Chapter10/Dollar.php
@@ -4,13 +4,13 @@ namespace TDD\Chapter10;
 
 class Dollar extends Money
 {	
-
+	
 	/**
-     * Instantiate the Dollar class with an amount value
-     * 
-     * @param int $amount
-     * @param string $currency
-     */
+	 * Instantiate the Dollar class with an amount value
+	 * 
+	 * @param int $amount
+	 * @param string $currency
+	 */
 	public function __construct(int $amount, string $currency)
 	{
 	    parent::__construct($amount, $currency);
