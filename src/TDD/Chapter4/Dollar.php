a9321e5c3a03b64dd70eca9cedc2f0ca2964fe8a
Logan Connor <loganconnor44@gmail.com>
2017-07-03 16:48:18 -0400

    attempting to fix strange spacing issue only found on github

--- src/TDD/Chapter4/Dollar.php
+++ src/TDD/Chapter4/Dollar.php
@@ -4,33 +4,33 @@ namespace TDD\Chapter4;
 
 class Dollar
 {
-	/**
+    /**
      * @var int $amount The monetary amount given to the dollar object
      */
-	private $amount;
+    private $amount;
 
-	/**
+    /**
      * Instantiate the dollar class with an amount value
      * 
      * @param int $amount
      */
-	public function __construct(int $amount)
-	{
-		$this->amount = $amount;
-	}
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
 
-	/**
+    /**
      * Takes a multipler to returns a new Dollar object instantiated with the product of the $amount and the passed in $multiplier
      * 
      * @param int $multiplier
      * @return object Dollar
      */
-	public function times(int $multiplier): Dollar
-	{
-		return new Dollar($this->amount * $multiplier); 
-	}
+    public function times(int $multiplier): Dollar
+    {
+        return new Dollar($this->amount * $multiplier); 
+    }
 
-	/**
+    /**
      * Take a Dollar object and returns a boolean if the passed in Dollar's $amount matches the self $amount
      * @todo remove redundant renaming of $object to $dollar
      * @todo capitalize $dollar for better readability to show it is an object
@@ -38,9 +38,9 @@ class Dollar
      * @param Dollar $object
      * @return bool
      */
-	public function equals(Dollar $object): bool
-	{
-		$dollar = $object;
-		return $this->amount == $dollar->amount; 
-	}
+    public function equals(Dollar $object): bool
+    {
+        $dollar = $object;
+        return $this->amount == $dollar->amount; 
+    }
 }
