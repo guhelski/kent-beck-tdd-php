e8ea042dd766b2331f387e604e8c82fa106f5517
Logan Connor <loganconnor44@gmail.com>
2017-07-05 21:56:12 -0400

    attempting to fix strange spacing issue only found on github

--- tests/TDD/Chapter5/Chapter5Test.php
+++ tests/TDD/Chapter5/Chapter5Test.php
@@ -5,36 +5,36 @@ namespace TDD\Chapter5;
 class Chapter5Test extends \PHPUnit_Framework_TestCase
 {
 
-	/**
-	 * Tests that the object $product's method amount returns the expected value
-	 */
-	public function testMultiplication()
-	{
-		$five = new Dollar(5);
-		$this->assertEquals(new Dollar(10), $five->times(2));
-		$this->assertEquals(new Dollar(15), $five->times(3));
-	}
+    /**
+     * Tests that the object $product's method amount returns the expected value
+     */
+    public function testMultiplication()
+    {
+        $five = new Dollar(5);
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
+    }
 
-	/**
-	 * Using Dollar's method equal, this tests that both objects are equal to another returning a true
-	 */
-	public function testEquality()
-	{
-		$fiveA = new Dollar(5);
-		$fiveB = new Dollar(5);
-		$this->assertTrue($fiveA->equals($fiveB));
+    /**
+     * Using Dollar's method equal, this tests that both objects are equal to another returning a true
+     */
+    public function testEquality()
+    {
+        $fiveA = new Dollar(5);
+        $fiveB = new Dollar(5);
+        $this->assertTrue($fiveA->equals($fiveB));
 
-		$six = new Dollar(6);
-		$this->assertFalse($fiveA->equals($six));
-	}
+        $six = new Dollar(6);
+        $this->assertFalse($fiveA->equals($six));
+    }
 
-	/**
-	 * Same test as the original using Franc, rather than Dollar
-	 */
-	public function testFrancMultiplication()
-	{
-		$five = new Franc(5);
-		$this->assertEquals(new Franc(10), $five->times(2));
-		$this->assertEquals(new Franc(15), $five->times(3));
-	}
+    /**
+     * Same test as the original using Franc, rather than Dollar
+     */
+    public function testFrancMultiplication()
+    {
+        $five = new Franc(5);
+        $this->assertEquals(new Franc(10), $five->times(2));
+        $this->assertEquals(new Franc(15), $five->times(3));
+    }
 }
